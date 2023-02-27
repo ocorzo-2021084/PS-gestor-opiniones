8a7cb767a8d761e2fe4daefafe1a2ea2aebd69d9
Octavio Corzo <ocorzo-2021084@kinal.edu.gt>
2023-02-27 16:48:22 -0600

    Se sube el proyecto corregido

---
+++ db/config.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+
+const dbConection = async () => {
+    try {
+
+        await mongoose.connect(process.env.DB_CNN);
+
+        console.log('Db Online');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error al inicializar la DB');
+    }
+}
+
+module.exports = {
+    dbConection
+}
